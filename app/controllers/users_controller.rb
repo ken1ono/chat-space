9c70d2709c30ae29b6f3106b0373306ea7871212
ken <onokenichi@onokenichi-no-MacBook-Pro.local>
2017-10-30 21:38:15 +0900

    ユーザー情報の追加_2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,17 @@
 class UsersController < ApplicationController
-
-  before_action :find_id, except: :users_params
-
   def edit
   end
 
   def update
-    @users.update(users_params)
-    redirect_to messages_path
+    if  current_user.update(users_params) then
+      redirect_to messages_path
+    else
+      redirect_to edit_user_path
+    end
   end
 
   private
     def users_params
     params.require(:user).permit(:name, :email)
-    end
-
-    def find_id
-      @users = User.find(params[:id])
     end
   end
