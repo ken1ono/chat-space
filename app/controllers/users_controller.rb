72e90c253604ddee8ea23284c14a6a067f37ce48
ken <onokenichi@onokenichi-no-MacBook-Pro.local>
2017-10-31 12:46:17 +0900

    サインアウト/サインイン機能の追加_1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,12 @@
 class UsersController < ApplicationController
+ before_action :authenticate_user!
+
   def edit
   end
 
   def update
     if  current_user.update(users_params) then
-      redirect_to messages_path
+        redirect_to messages_path
     else
       render :edit
     end
