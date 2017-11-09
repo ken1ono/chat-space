685c9d2d4b9e3b5c85067662471e6220a5f092e6
ken <onokenichi@onokenichi-no-MacBook-Pro.local>
2017-11-09 16:13:36 +0900

    モデルのテスト_4

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -2,10 +2,21 @@ require 'rails_helper'
 
 describe Message do
   describe '#create' do
-    it "is valid with a body, image, user_id, group_id" do
+    it "is valid with a body" do
+      message = build(:message, image: nil)
+      expect(message).to be_valid
+    end
+
+    it "is valid with a image" do
+      message = build(:message, body: nil)
+      expect(message).to be_valid
+    end
+
+    it "is valid with a body, image" do
       message = build(:message)
       expect(message).to be_valid
     end
+
 
     it "is invalid without a body, user_id, group_id " do
       message = build(:message, body: nil, image: nil)
