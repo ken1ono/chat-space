9c2bf81b3220dcdf93b702a5b81a01167c967013
ken <onokenichi@onokenichi-no-MacBook-Pro.local>
2017-11-09 15:35:26 +0900

    モデルのテスト_3

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -2,35 +2,25 @@ require 'rails_helper'
 
 describe Message do
   describe '#create' do
-    it "is valid with a body, user_id, group_id" do
-      message = build(:message)
-      expect(message).to be_valid
-    end
-
-    it "is valid with image" do
-      message = build(:message)
-      expect(message).to be_valid
-    end
-
-    it "is valid with a body, image" do
+    it "is valid with a body, image, user_id, group_id" do
       message = build(:message)
       expect(message).to be_valid
     end
 
     it "is invalid without a body, user_id, group_id " do
-      message = Message.new(body: "", image: "")
+      message = build(:message, body: nil, image: nil)
       message.valid?
       expect(message.errors[:body_or_image]).to include("Body or imageを入力してください")
     end
 
     it "is invalid without a group_id" do
-      message = Message.new(group_id: "")
+      message = build(:message, group_id: nil)
       message.valid?
       expect(message.errors[:group]).to include("を入力してください")
     end
 
     it "is invalid without a user_id" do
-      message = Message.new(user_id: "")
+      message = build(:message, user_id: nil)
       message.valid?
       expect(message.errors[:user]).to include("を入力してください")
     end
